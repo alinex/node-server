1df745a08559e39291081bc03477213fa1885753
Alinex <info@alinex.de>
2016-02-29 14:29:22 +0100

    Updated documentation

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"src":{"files":["configSchema.coffee","index.coffee"],"dirs":{"http":{"files":["index.coffee"],"dirs":{"plugin":{"files":["log.coffee"]}}}}},"var":{"dirs":{"src":{"dirs":{"config":{"dirs":{"server":{"dirs":{"http":{"files":["index.yml"]}}}}}}}}}}};
+var tree={"files":["Changelog.md","README.md","coffeelint.json","package.json"],"dirs":{"src":{"files":["configSchema.coffee","index.coffee"],"dirs":{"http":{"files":["index.coffee"],"dirs":{"plugin":{"files":["debug.coffee","log.coffee"]}}}}}}};
