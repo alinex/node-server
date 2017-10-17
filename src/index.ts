95fbe54dfb08ca162be1c3240b88db1622652489
alinex <info@alinex.de>
2017-10-17 21:53:27 +0200

    Implement ts testing & co

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // greeter.ts
-function greeter (person: string) {
+function greeter(person: string) {
   return `Hello ${person}!`
 }
 
-const userName = 'Node Hero'
+const userName = "Node Hero"
 
+// tslint:disable-next-line
 console.log(greeter(userName))
